e978dbbcb5ff3238d923e05978f3f4a4fd9d0840
Dodong2 <carlseighartaliodegmail.com>
2024-04-10 22:59:22 +0800

    added Todo list

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // multiplication
 
-const res = document.querySelector('.res')
-const btn = document.querySelector('.btn1')
+const res = document.querySelector(".res");
+const btn = document.querySelector(".btn1");
 
-btn.addEventListener('click', function(){
-    const num1 = parseInt(document.querySelector('.num1').value)
-    const num2 = parseInt(document.querySelector('.num2').value)
+btn.addEventListener("click", function () {
+  const num1 = parseInt(document.querySelector(".num1").value);
+  const num2 = parseInt(document.querySelector(".num2").value);
 
+  let output = "";
+  for (let i = 1; i <= num2; i++) {
+    let calcu = num1 * i;
+    output += `${num1} x ${i} = ${calcu}<br>`;
+  }
+  res.innerHTML = output;
+});
 
-    let output = ''
-    for(let i = 1; i <= num2; i++) {
-        let calcu = num1 * i;
-        output += `${num1} x ${i} = ${calcu}<br>`
-    }
-    res.innerHTML = output
-})
+const todo = document.querySelector(".todo");
+const btn2 = document.querySelector(".btn2");
+const list = document.querySelector(".lists");
+
+btn2.addEventListener("click", () => {
+  const todoTxt = todo.value.trim();
+  if (todoTxt != "") var item = document.createElement("li");
+  item.textContent = todoTxt;
+  list.appendChild(item);
+  todo.value = "";
+});
