9143f1f1e0969d136e8e5acc462a8af130b2d6ba
Dodong2 <carlseighartaliodegmail.com>
2024-04-10 23:14:30 +0800

    added new

--- index.js
+++ index.js
@@ -4,25 +4,54 @@ const res = document.querySelector(".res");
 const btn = document.querySelector(".btn1");
 
 btn.addEventListener("click", function () {
-  const num1 = parseInt(document.querySelector(".num1").value);
-  const num2 = parseInt(document.querySelector(".num2").value);
+    const num1 = parseInt(document.querySelector(".num1").value);
+    const num2 = parseInt(document.querySelector(".num2").value);
 
-  let output = "";
-  for (let i = 1; i <= num2; i++) {
-    let calcu = num1 * i;
-    output += `${num1} x ${i} = ${calcu}<br>`;
-  }
-  res.innerHTML = output;
+    let output = "";
+    for (let i = 1; i <= num2; i++) {
+        let calcu = num1 * i;
+        output += `${num1} x ${i} = ${calcu}<br>`;
+    }
+    res.innerHTML = output;
 });
 
+
+// Todo basic 1
+// const todo = document.querySelector(".todo");
+// const btn2 = document.querySelector(".btn2");
+// const list = document.querySelector(".lists");
+
+// btn2.addEventListener("click", () => {
+//   const todoTxt = todo.value.trim();
+//   if (todoTxt != "") 
+//   var item = document.createElement("li");
+//   item.textContent = todoTxt;
+//   list.appendChild(item);
+//   todo.value = "";
+
+
+// });
+
+// Todo basic 2 with button delete
 const todo = document.querySelector(".todo");
 const btn2 = document.querySelector(".btn2");
 const list = document.querySelector(".lists");
 
 btn2.addEventListener("click", () => {
-  const todoTxt = todo.value.trim();
-  if (todoTxt != "") var item = document.createElement("li");
-  item.textContent = todoTxt;
-  list.appendChild(item);
-  todo.value = "";
+    const todoTxt = todo.value.trim();
+    if (todoTxt != "")
+    var item = document.createElement("li");
+    item.textContent = todoTxt;
+
+    var delBtn = document.createElement('button')
+    delBtn.textContent = "Delete"
+    delBtn.addEventListener('click', () => {
+        item.remove()
+    })
+    list.appendChild(item);
+
+    item.appendChild(delBtn)
+    todo.value = "";
+
 });
+
